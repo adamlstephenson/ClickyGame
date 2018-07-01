61fd7da5d4b2841a44c833feca5f18c4f4b52619
Adam Stephenson <astephensonx@gmail.com>
2018-06-30 19:11:23 -0500

    first part of basic styling done

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,12 +5,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li className="App-name">
+              <a href="/">Clicky Game</a>
+            </li>
+            <li className="guess-notification">
+              Click an Image to Begin!
+            </li>
+            <li className="score-keeper">
+              Score: 0 | Top Score: 0
+            </li>
+          </ul>
+        </nav>
         <header className="App-header">
-          <h1 className="App-title">Clicky Game!</h1>
+          <h1>Clicky Game!</h1>
+          <h2>Click an image to earn points, but don't click on any more than once!</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
