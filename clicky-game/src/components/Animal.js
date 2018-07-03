75f26ad466c151ba84720161f4fe8bf273eedd7b
Adam Stephenson <astephensonx@gmail.com>
2018-07-02 20:00:32 -0500

    click functionality back to working

--- clicky-game/src/components/Animal.js
+++ clicky-game/src/components/Animal.js
@@ -4,22 +4,6 @@ import animals from "../animals.json";
 
 class Animal extends Component {
 
-    state = {
-        animals: animals,
-        score: 0,
-        topScore: 0,
-        headline: "Click an Image to Begin!",
-        clicked: "false"
-      }
-
-      imageClick = (props) => {
-
-        this.setState({headline: "You Guessed Correctly!"});
-        this.setState({score: this.state.score +1});
-        this.setState({topScore: this.state.topScore +1})
-        this.setState({clicked: "true"}); 
-        }
-
     render() {
         return (
             <main>
