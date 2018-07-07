aa9e2eb70c8cfd7f65e10026592520415794793c
Adam Stephenson <astephensonx@gmail.com>
2018-07-07 02:15:11 -0500

    fully functional

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import animals from "./animals.json";
+import _ from "underscore";
 
-const clicked = [2];
+
+const clicked = [];
 
 class App extends Component {
 
   state = {
-    animals: animals,
+    animals: _.shuffle(animals),
     score: 0,
     topScore: 0,
     headline: "Click an Image to Begin!",
@@ -15,21 +17,33 @@ class App extends Component {
 
   imageClick = event => {
 
-    console.log(event.target.id)
+    const click = Number(event.target.id);
 
-    if (clicked.includes(2)) {
-      
-      this.setState({headline: "You Guessed Incorrectly"});
+    if (clicked.includes(click)) {
+
+      this.setState({
+        headline: "You Guessed Incorrectly",
+        score: 0,
+        topScore: (this.state.score > this.state.topScore) ? this.state.score : this.state.topScore
+      });
+
+      const clicked = [];
+
     }
     else {
 
-      this.setState({score: this.state.score +1})
-      this.setState({headline: "You Guessed Correctly!"});
-      this.setState({topScore: this.state.topScore +1});
+      clicked.push(click);
+      this.setState({
+        headline: "You Guessed Correctly!",
+        score: this.state.score + 1,
+      })
     }
+
+    this.setState({ animals: _.shuffle(animals), })
+
   }
-  
-  
+
+
   render() {
 
     return (
@@ -52,15 +66,15 @@ class App extends Component {
           <h2>Click an image to earn points, but don't click on any more than once!</h2>
         </header>
         <main className="container">
-        {this.state.animals.map((animal) => (
-          <div key={animal.id} className="image-holder" onClick={this.imageClick}>
-            <img src={animal.source} alt={animal.name} id={animal.id}/>
-          </div>
-        ))}
+          {this.state.animals.map((animal) => (
+            <div key={animal.id} className="image-holder" onClick={this.imageClick}>
+              <img src={animal.source} alt={animal.name} id={animal.id} />
+            </div>
+          ))}
         </main>
         <footer className="footer">
           <div className="bottom">
-            Clicky Game 
+            Clicky Game
            {/* <img src="./images/react.png" alt="react"/> */}
           </div>
         </footer>
